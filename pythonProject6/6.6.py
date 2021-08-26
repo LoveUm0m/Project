3b126172613d2dc3069f816e8e51ab5513ffdcab
Vladislav <vlad.htcx97@gmai.com>
2021-08-26 20:13:11 +0300

    homwork

--- pythonProject6/6.6.py
+++ pythonProject6/6.6.py
@@ -1,12 +1,4 @@
 import sys
-price = sys.argv[1]
-with open('bakery.csv', 'a', encoding='utf-8') as f:
-    f.write(price + '\n')
-
-
-
-
-import sys
 nums = sys.argv[1:]
 with open('bakery.csv', encoding='utf-8') as f:
     if len(nums) > 1:
--- pythonProject6/6.7.py
+++ pythonProject6/6.7.py
@@ -2,18 +2,20 @@ with open('bakery.csv', 'w') as f:
     f.write('5978,5\n8914,3\n7879,1\n1573,7')
 
 import sys
-edit, new_val = sys.argv[1:]
+
+editid, new_val = sys.argv[1:]
 with open('bakery.csv', 'r+') as f:
     tmp = open('bakery.tmp', 'w+')
     change = False
     for i, line in enumerate(f, 1):
-        if i == int(edit):
+
+        if i == int(editid):
             tmp.write(f'{new_val}\n')
-            change - True
+            change = True
         else:
             tmp.write(line)
     if not change:
-        exit
+        exit('error')
 
     tmp.seek(0)
 
